8904feae7197e68fb6ae4ca296b287c548483cf2
Avs163 <achintya.veer1999@gmail.com>
2021-02-28 13:34:35 +0530

    Adding files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'SHA';
+  value = '';
+  constructor() { }
+
+  addTodo(value:string) {
+    var abc = require('sha256');
+    // console.log(abc(value));
+    this.value = abc(value);
+  }
 }

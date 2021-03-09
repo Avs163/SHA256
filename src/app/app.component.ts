622cdcc688f21d90d6a0933197c0d907e39f771e
Avs163 <achintya.veer1999@gmail.com>
2021-03-09 22:40:44 +0530

    Reinitialized sha256 algo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Sha256 } from './sha';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent{
   title = 'SHA';
   value = '';
+
   constructor() { }
 
-  addTodo(value:string) {
-    var abc = require('sha256');
+
+
+  addTodo(message:string) {
+    // var abc = require('sha256');
     // console.log(abc(value));
-    this.value = abc(value);
+    this.value = Sha256.hash(message);
   }
+
 }
+
+
